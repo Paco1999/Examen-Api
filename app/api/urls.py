7680de3436108e5e146059944aa71fbdbf987b09
Paco1999 <paco991121@hotmail.com>
2022-02-23 17:09:07 -0600

    first commit

---
+++ app/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import include,path
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'movies', views.MovieViewSet)
+router.register(r'moviecategories', views.MovieCategoriesViewSet)
+router.register(r'actores', views.ActorsViewSet)
+router.register(r'directores', views.FilmDirectorViewSet)
+
+
+urlpatterns = [
+    #path('', views.index, name='index'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
